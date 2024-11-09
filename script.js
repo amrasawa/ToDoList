ae7175d0dc8e6ebbaa922784dfe06bec44bd468f
amrasawa <karmelsalma5@gmail.com>
2024-11-09 14:27:38 +0200

    filling the list with tasks from API

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+let tasks=document.querySelector(".tasks");
 async function ToDos(){
     let response =await fetch("https://dummyjson.com/todos");
     let data=await response.json();
     //console.log(data.todos);
     let result =data.todos.map((task)=>{
-        
-    })
+        return `
+            <tr>
+                <td>${task.id}</td>
+                <td>${task.todo}</td>
+                <td>${task.userId}</td>
+                <td>${(task.completed)? "completed": "Pending"}
+                <td> <button class="delete button">Delete</button><button class="Done button">Done</button></td>
+            </tr>`
+    }).join('');
+    tasks.innerHTML+=result;
+
 }
 ToDos();
